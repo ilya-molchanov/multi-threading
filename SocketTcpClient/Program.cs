6b5224ed4535bce52fbf2ea13d305448044d82d9
ilya.molchanov <ilya.molchanov@aisnovations.com>
2019-03-05 00:31:08 +0300

    added client and server

---
+++ SocketTcpClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketTcpClient
+{
+    class Program
+    {
+        static int port = 8005; // server port
+        static string address = "127.0.0.1"; // server address
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // connect to the remote host
+                socket.Connect(ipPoint);
+                Console.Write("enter message");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                socket.Send(data);
+
+                // getting response
+                data = new byte[256]; // response buffer
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // received bytes count
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                Console.WriteLine("server response: " + builder.ToString());
+
+                // close socket
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SocketTcpServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketTcpServer
+{
+    class Program
+    {
+        static int port = 8005; // incoming port
+        static void Main(string[] args)
+        {
+            // get addressess for starting socket
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            // create socket
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                // bind socket with local incoming data point
+                listenSocket.Bind(ipPoint);
+
+                // starting listening
+                listenSocket.Listen(10);
+                Console.WriteLine("Server started. Waiting connections...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    // get message
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // bytes count
+                    byte[] data = new byte[256]; // buffer
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (handler.Available > 0);
+
+                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                    // send response
+                    string message = "message delivered";
+                    data = Encoding.Unicode.GetBytes(message);
+                    handler.Send(data);
+                    // close socket
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
