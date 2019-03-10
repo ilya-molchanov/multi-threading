aa0bc5d215d319c896ac9d979492551442d8d021
ilya.molchanov <ilya.molchanov@aisnovations.com>
2019-03-11 01:29:04 +0300

    update

--- SocketTcpClient/Program.cs
+++ SocketTcpClient/Program.cs
@@ -1,53 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SocketTcpClient
 {
     class Program
     {
-        static int port = 8005; // server port
-        static string address = "127.0.0.1"; // server address
+        //Function to get random number
+        private static readonly Random _getrandom = new Random();
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (_getrandom) // synchronize
+            {
+                return _getrandom.Next(min, max);
+            }
+        }
+
+        private static readonly int _port = 8005; // server _port
+        private static readonly string _address = "127.0.0.1"; // server _address
+
+        private static List<string> _clientNamesList = new List<string>() {"first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eigth", "nineth", "tenth" };
+
         static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(state => ClientSendMessages(Thread.CurrentThread.ManagedThreadId));
+            ThreadPool.QueueUserWorkItem(state => ClientSendMessages(Thread.CurrentThread.ManagedThreadId));
+            ThreadPool.QueueUserWorkItem(state => ClientSendMessages(Thread.CurrentThread.ManagedThreadId));
+
+            Console.Read();
+        }
+        
+        private static async Task ClientSendMessages(int numberClient)
         {
             try
             {
-                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
-
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                // connect to the remote host
-                socket.Connect(ipPoint);
-                Console.Write("enter message");
-                string message = Console.ReadLine();
-                byte[] data = Encoding.Unicode.GetBytes(message);
-                socket.Send(data);
-
-                // getting response
-                data = new byte[256]; // response buffer
-                StringBuilder builder = new StringBuilder();
-                int bytes = 0; // received bytes count
-
-                do
+                for (int i = 0; i < GetRandomNumber(2, 3); i++)
                 {
-                    bytes = socket.Receive(data, data.Length, 0);
-                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                }
-                while (socket.Available > 0);
-                Console.WriteLine("server response: " + builder.ToString());
+                    IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(_address), _port);
 
-                // close socket
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    // connect to the remote host
+                    socket.Connect(ipPoint);
+                    string sendingMessage = _clientNamesList[GetRandomNumber(0, 9)];
+                    byte[] data = Encoding.Unicode.GetBytes(numberClient + "|" + sendingMessage);
+                    socket.Send(data);
+
+                    // getting response
+                    data = new byte[10000]; // response buffer
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // received bytes count
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socket.Available > 0);
+
+                    if (builder.ToString() != string.Empty)
+                    {
+                        Console.WriteLine("client number: " + numberClient + " got server response: " +
+                                          builder.ToString());
+                    }
+
+                    // close socket
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            Console.Read();
+            await Task.Delay(2000);
         }
     }
 }
--- SocketTcpServer/Program.cs
+++ SocketTcpServer/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 namespace SocketTcpServer
 {
     class Program
     {
-        static int port = 8005; // incoming port
+        public class InnerSendingThreadsStructure
+        {
+            public InnerSendingThreadsStructure(string thread, bool isSendingThread)
+            {
+                this.thread = thread;
+                this.isSendingThread = isSendingThread;
+            }
+
+            public InnerSendingThreadsStructure()
+            {
+            }
+
+            public string thread { get; set; }
+            public bool isSendingThread { get; set; }
+        }
+
+        private static Dictionary<string, Dictionary<string, List<InnerSendingThreadsStructure>>> _history =
+            new Dictionary<string, Dictionary<string, List<InnerSendingThreadsStructure>>>();
+
+        private static int port = 8005; // incoming port
+
         static void Main(string[] args)
         {
             // get addressess for starting socket
@@ -31,25 +52,58 @@ namespace SocketTcpServer
                     // get message
                     StringBuilder builder = new StringBuilder();
                     int bytes = 0; // bytes count
-                    byte[] data = new byte[256]; // buffer
+                    byte[] data = new byte[10000]; // buffer
 
                     do
                     {
                         bytes = handler.Receive(data);
                         builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                    }
-                    while (handler.Available > 0);
+                    } while (handler.Available > 0);
 
-                    Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+                    String[] transferredArgs = builder.ToString().Split('|');
+                    Console.WriteLine(DateTime.Now.ToLongTimeString() + " thread number : " + transferredArgs[0] +
+                                      " sent message " + transferredArgs[1]);
+                    Dictionary<string, List<InnerSendingThreadsStructure>> helpingDictionary =
+                        new Dictionary<string, List<InnerSendingThreadsStructure>>();
+                    List<InnerSendingThreadsStructure> helpingList = new List<InnerSendingThreadsStructure>();
+                    helpingList.Add(new InnerSendingThreadsStructure(transferredArgs[0], true));
 
+                    helpingDictionary.Add(transferredArgs[1], helpingList);
+
+                    _history.Add(Guid.NewGuid().ToString(), helpingDictionary);
                     // send response
-                    string message = "message delivered";
-                    data = Encoding.Unicode.GetBytes(message);
-                    handler.Send(data);
+                    SendMessagesToClients(transferredArgs[0], handler);
                     // close socket
                     handler.Shutdown(SocketShutdown.Both);
                     handler.Close();
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+
+        private static void SendMessagesToClients(string numberThread, Socket handler)
+        {
+            try
+            {
+                byte[] data = new byte[10000]; // buffer
+                string sendingData = string.Empty;
+                foreach (string key in _history.Keys)
+                {
+                    foreach (var innerDictionary in _history[key])
+                    {
+                        if (!innerDictionary.Value.Any(x => x.thread == numberThread))
+                        {
+                            innerDictionary.Value.Add(new InnerSendingThreadsStructure(numberThread, false));
+                            string sendingThread = innerDictionary.Value.Single(x => x.isSendingThread).thread;
+                            sendingData += "Other thread number : " + sendingThread + " sent message " + innerDictionary.Key + Environment.NewLine;
+                        }
+                    }
+                }
+                handler.Send(Encoding.Unicode.GetBytes(sendingData));
             }
             catch (Exception ex)
             {
